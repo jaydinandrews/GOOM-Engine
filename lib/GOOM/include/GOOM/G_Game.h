05c684c81631875b43c1b4b242c48a2a1c0b246c
Jaydin Andrews <jaydin.andrews@gmail.com>
2023-07-22 01:43:40 -0400

    add game logic steps

--- lib/GOOM/include/GOOM/G_Game.h
+++ lib/GOOM/include/GOOM/G_Game.h
@@ -1,18 +1,38 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "G_Settings.h"
 #include <stdint.h>
+#include <SDL2/SDL_timer.h>
+
+#define GAME_STATE_INIT 0
+#define GAME_STATE_MENU 1
+
+#define KEY_COUNT 16
+#define MS_PER_FRAME (1000 / TARGET_FPS)
 
 typedef struct game_s {
+
+  uint8_t state; // current game state
+  uint32_t state_time; // time since last state change
+  uint8_t key_state[KEY_COUNT];
 
   uint32_t frame; // current frame number
   uint32_t frame_time; // time of current frame start
   uint8_t current_random; // seed for RNG
   uint8_t running; // whether game continues or exits
+
+  uint16_t ray_constraint_max_hits;
+  uint16_t ray_constraint_max_steps;
+  uint16_t visibility_ray_constraint_max_hits;
+  uint16_t visibility_ray_constraint_max_steps;
+
+  uint8_t selected_menu_item;
 } game_t;
 
 void G_InitGame(game_t *game);
-
-uint8_t G_GameMainLoop();
+void G_SetGameState(game_t *game, uint8_t state);
+uint8_t G_GameMainLoop(game_t *game);
+void G_GameStep(game_t *game);
 
 #endif /* GAME_H */
--- lib/GOOM/src/G_Game.c
+++ lib/GOOM/src/G_Game.c
@@ -1,12 +1,64 @@
 #include "G_Game.h"
+#include "M_Menu.h"
 
 void G_InitGame(game_t *game) {
   game->frame = 0;
   game->frame_time = 0;
   game->current_random = 0;
   game->running = 1;
+
+  game->ray_constraint_max_hits = RAYCAST_MAX_HITS;
+  game->ray_constraint_max_steps = RAYCAST_MAX_STEPS;
+  game->visibility_ray_constraint_max_hits = RAYCAST_VISIBILITY_MAX_HITS;
+  game->visibility_ray_constraint_max_steps = RAYCAST_VISIBILITY_MAX_STEPS;
+
+  // Average texture colors
+  // Background scale map
+  // Key state
+  // Try to load saved game
+  // Set music on/off
+  G_SetGameState(game, GAME_STATE_INIT);
 }
 
-uint8_t G_GameMainLoop(){
-  return 1;
+void G_SetGameState(game_t *game, uint8_t state) {
+  game->state = state;
+  game->state_time = 0;
+}
+
+uint8_t G_GameMainLoop(game_t *game) {
+  if (game->state != GAME_STATE_INIT) {
+    uint32_t time_current = SDL_GetTicks();
+    int32_t time_since_last_frame = time_current - game->frame_time;
+    if (time_since_last_frame >= MS_PER_FRAME) {
+      uint8_t steps = 0;
+      uint8_t was_first_frame = game->frame == 0;
+      while (time_since_last_frame >= MS_PER_FRAME) {
+        // get current weapon
+        game->frame_time += MS_PER_FRAME;
+        G_GameStep(game);
+        // check and/or set new weapon
+        time_since_last_frame -= MS_PER_FRAME;
+        game->frame++;
+        steps++;
+      }
+      // if((steps > 1) && (!was_first_frame)) then failed to reach target fps
+      //  TODO: Draw
+    }      // else: wait and relieve CPU
+  } else { // if in GAME_STATE_INIT
+    G_SetGameState(game, GAME_STATE_MENU);
+  }
+  return game->running;
+}
+
+void G_GameStep(game_t *game) {
+  //TODO: get key state for changes
+
+  switch(game->state) {
+    case GAME_STATE_MENU:
+      M_GameStepMenu(game);
+      break;
+    default:
+      break;
+  }
+  game->state_time += MS_PER_FRAME;
 }
