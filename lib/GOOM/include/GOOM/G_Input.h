f5fb924b59c377a373a47c6dac50d0ee4cbc2420
Jaydin Andrews <jaydin.andrews@gmail.com>
2023-07-23 04:36:16 -0400

    add input key check

--- lib/GOOM/include/GOOM/G_Input.h
+++ lib/GOOM/include/GOOM/G_Input.h
@@ -21,5 +21,6 @@
 uint16_t G_KeyRegisters(game_t *game, uint8_t key);
 uint8_t G_KeyJustPressed(game_t *game, uint8_t key);
 uint8_t G_KeyRepeated(game_t *game, uint8_t key);
+uint8_t G_KeyIsDown(game_t *game, uint8_t key);
 
 #endif /* INPUT_H */
--- lib/GOOM/src/G_Input.c
+++ lib/GOOM/src/G_Input.c
@@ -12,3 +12,7 @@ uint8_t G_KeyRepeated(game_t *game, uint8_t key) {
   //TODO
   return 0;
 }
+
+uint8_t G_KeyIsDown(game_t *game, uint8_t key) {
+  return game->key_state[key] != 0;
+}
