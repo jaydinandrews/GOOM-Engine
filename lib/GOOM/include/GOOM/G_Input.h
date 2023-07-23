b7ce594583fb47dff2ae408003085420797d1bca
Jaydin Andrews <jaydin.andrews@gmail.com>
2023-07-22 21:18:48 -0400

    add level selection

--- lib/GOOM/include/GOOM/G_Input.h
+++ lib/GOOM/include/GOOM/G_Input.h
@@ -1,13 +1,25 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-#include "G_Settings.h"
 #include "G_Game.h"
-#include "M_Menu.h"
+
+#include <stdint.h>
+
+#define KEY_UP 0
+#define KEY_RIGHT 1
+#define KEY_DOWN 2
+#define KEY_LEFT 3
+#define KEY_PRIMARY 4
+#define KEY_SECONDARY 5
+#define KEY_TERTIARY 6
+#define KEY_JUMP 7
+#define KEY_LEFT_STRAFE 8
+#define KEY_RIGHT_STRAFE 9
+#define KEY_MAP 10
+#define KEY_MENU 11
 
 uint16_t G_KeyRegisters(game_t *game, uint8_t key);
 uint8_t G_KeyJustPressed(game_t *game, uint8_t key);
 uint8_t G_KeyRepeated(game_t *game, uint8_t key);
-
 
 #endif /* INPUT_H */
--- lib/GOOM/src/M_Menu.c
+++ lib/GOOM/src/M_Menu.c
@@ -1,5 +1,7 @@
 #include "M_Menu.h"
-#include <stdint.h>
+#include "G_Game.h"
+#include "G_Input.h"
+#include "G_Level.h"
 
 void M_GameStepMenu(game_t *game) {
   uint8_t menu_items = 0;
@@ -7,7 +9,50 @@ void M_GameStepMenu(game_t *game) {
     menu_items++;
   }
 
-  uint8_t item = M_GetMenuItem(game->selected_menu_item);
+  uint8_t item = M_GetMenuItem(game->current_menu_item);
+
+  if(G_KeyRegisters(game, KEY_DOWN) && (game->current_menu_item < menu_items -1)) {
+    game->current_menu_item++;
+    //TODO: play sound
+  } else if(G_KeyRegisters(game, KEY_UP) && (game->current_menu_item > 0)) {
+    game->current_menu_item--;
+    //TODO: play sound
+  } else if(G_KeyJustPressed(game, KEY_PRIMARY)) {
+    switch (item) {
+      case MENU_ITEM_PLAY:
+        for(uint8_t i = 6; i < SAVE_SIZE; ++i) {
+          game->save[i] = 0;
+        }
+        if(game->current_level == 0) {
+          G_SetGameState(game, GAME_STATE_INTRO);
+        } else {
+          G_InitLevel(game, game->current_level);
+        }
+        break;
+      case MENU_ITEM_LOAD:
+        //TODO
+        break;
+      case MENU_ITEM_CONTINUE:
+        G_SetGameState(game, GAME_STATE_PLAYING);
+        break;
+      case MENU_ITEM_SOUND:
+        //TODO
+        break;
+      case MENU_ITEM_EXIT:
+        game->running = 0;
+        break;
+      default:
+        break;
+    }
+  } else if(item == MENU_ITEM_PLAY) {
+    if(G_KeyRegisters(game, KEY_RIGHT) && (game->current_level < (game->save[0] & 0x0F))) {
+      game->current_level++;
+      //TODO: play sound
+    } else if (G_KeyRegisters(game, KEY_LEFT) && (game->current_level > 0)) {
+      game->current_level--;
+      //TODO: play sound
+    }
+  }
 }
 
 uint8_t M_GetMenuItem(uint8_t index) {
